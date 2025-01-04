a463e801532bcf773675dd215925f829c8b09291
yuri glecios <glleciosyuri@gmail.com>
2025-01-04 17:09:17 -0300

    Criação do projeto

---
+++ src/main/java/br/com/bidup/BidupPlataformApplication.java
@@ -0,0 +1,13 @@
+package br.com.bidup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidupPlataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidupPlataformApplication.class, args);
+	}
+
+}
